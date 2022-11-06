4542eef5a7704b8544fd64f20ffc65e0def5495e
cfowl <goose_7@msn.com>
2022-11-05 21:06:38 -0600

    first commit

---
+++ routes/auth.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const passport = require('passport');
+const router = express.Router();
+
+// @desc    Auth with Google
+// @route   GET /auth/google
+router.get('/google', passport.authenticate('google', { scope: ['profile'] }));
+
+// @desc    Google Auth callback
+// @route   GET /auth/google/callback
+router.get('/google/callback', passport.authenticate('google', { failureRedirect: '/' }), (req, res) => {
+    res.redirect('/dashboard');
+});
+
+// @desc    Logout user
+// @route   POST /auth/logout
+router.get('/logout', (req, res, next) => {
+    // good idea to use POST or DELETE instead of GET for logging out
+    req.logout((err) => {
+        if(err) return next(err);
+        res.redirect('/');
+    });
+});
+
+module.exports = router;
---
+++ routes/scrolls.js
@@ -0,0 +1,133 @@
+const express = require('express');
+const router = express.Router();
+const { ensureAuth } = require('../middleware/auth');
+
+const Scroll = require('../models/scroll');
+
+// @desc    Show add page
+// @route   GET /scrolls/add
+router.get('/add', ensureAuth, (req, res) => {
+    res.render('scrolls/add');
+});
+
+// @desc    Process add form
+// @route   POST /scrolls
+router.post('/', ensureAuth, async (req, res) => {
+    try {
+        req.body.user = req.user.id;
+        await Scroll.create(req.body);
+        res.redirect('/dashboard');  
+    } catch (err) {
+        console.error(err);
+        res.render('error/500');
+    }
+});
+
+// @desc    Show all scrolls
+// @route   GET /scrolls
+router.get('/', ensureAuth, async (req, res) => {
+    try {
+        const scrolls = await Scroll.find({ status: 'public' })
+            .populate('user')
+            .sort({ createdAt: 'desc' })
+            .lean();
+
+        res.render('scrolls/index', { scrolls });
+    } catch (err) {
+        console.error(err);
+        res.render('error/500');
+    }
+});
+
+// @desc    Show one scroll
+// @route   GET /scrolls/:id
+router.get('/:id', ensureAuth, async (req, res) => {
+    try {
+        const scroll = await Scroll.findById(req.params.id)
+          .populate('user')
+          .lean();
+        
+        if(!scroll) return res.render('error/404');
+
+        res.render('scrolls/show', { scroll });
+    } catch (err) {
+        console.error(err);
+        res.render('error/500');
+    }
+});
+
+// @desc    Show edit page
+// @route   GET /scrolls/edit/:id
+router.get('/edit/:id', ensureAuth, async (req, res) => {
+    try {
+        const scroll = await Scroll.findOne({ _id: req.params.id }).lean();
+        if(!scroll) return res.render('error/404');
+
+        if(scroll.user != req.user.id) res.redirect('/scrolls');
+        else res.render('scrolls/edit', { scroll });
+    } catch (err) {
+        console.error(err);
+        res.render('error/500');
+    }
+});
+
+// @desc    Update scroll
+// @route   PUT /scrolls/:id
+router.put('/:id', ensureAuth, async (req, res) => {
+    try {
+        let scroll = await Scroll.findById(req.params.id).lean();
+        if(!scroll) return res.render('error/404');
+
+        if(scroll.user != req.user.id) res.redirect('/scrolls');
+        else {
+            scroll = await Scroll.findOneAndUpdate({ _id: req.params.id }, req.body, {
+                new: true,
+                runValidators: true
+            });
+
+            res.redirect('/dashboard');
+        }
+    } catch (err) {
+        console.error(err);
+        res.render('error/500');
+    }
+});
+
+// @desc    Delete scroll
+// @route   DELETE /scrolls/:id
+router.delete('/:id', ensureAuth, async (req, res) => {
+    try {
+        const scroll = await Scroll.findById(req.params.id).lean();
+        if(!scroll) return res.render('error/404');
+
+        if(scroll.user != req.user.id) res.redirect('/scrolls');
+
+        else {
+            await Scroll.remove({ _id: req.params.id });
+            res.redirect('/dashboard');
+        }
+    } catch (err) {
+        console.error(err);
+        res.render('error/500');
+    }
+});
+
+// @desc    User scrolls
+// @route   GET /scrolls/user/:id
+router.get('/user/:userId', ensureAuth, async (req, res) => {
+    try {
+        const scrolls = await Scroll.find({
+            user: req.params.userId,
+            status: 'public'
+        })
+          .populate('user')
+          .lean();
+
+        res.render('scrolls/index', { scrolls });
+    } catch (err) {
+        console.error(err);
+        res.render('error/500');
+    }
+});
+
+module.exports = router;
