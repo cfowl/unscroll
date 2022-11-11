91c04df52672ed789942f895430d95b462397252
cfowl <goose_7@msn.com>
2022-11-10 21:21:18 -0700

    new users route

---
+++ routes/users.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+const { ensureAuth } = require('../middleware/auth');
+
+const User = require('../models/User');
+
+// @desc    Show all users
+// @route   GET /users
+router.get('/', ensureAuth, async (req, res) => {
+    try {
+        const users = await User.find()
+            .sort({ createdAt: 'desc' })
+            .lean();
+
+        res.render('users/index', {
+            users,
+            email: req.user.email
+        });
+    } catch (err) {
+        console.error(err);
+        res.render('error/500');
+    }
+});
+
+module.exports = router;
