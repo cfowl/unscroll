03e76de7784f5610485b6a737cddacf678080935
cfowl <goose_7@msn.com>
2022-12-20 20:51:36 -0700

    hardcoding timezone America/Denver

--- helpers/hbs.js
+++ helpers/hbs.js
@@ -2,10 +2,10 @@ const moment = require('moment');
 const momentTimezone = require('moment-timezone');
 
 module.exports = {
-    formatDate: (date, USERZONE) => {
+    formatDate: (date) => {
         // using Intl.DateTimeFormat().resolvedOptions().timeZone to get user timezone didn't work with Heroku
         // const userZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-        return moment(date).tz(USERZONE).format('lll');
+        return moment(date).tz('America/Denver').format('lll');
     },
     truncate: (str, len) => {
         if(str.length > len && str.length > 0) {
