b133231274f5db38f5c3c3db1881fbcfb79c6b0c
cfowl <goose_7@msn.com>
2022-12-20 20:54:55 -0700

    display timezone tag

--- helpers/hbs.js
+++ helpers/hbs.js
@@ -5,7 +5,7 @@ module.exports = {
     formatDate: (date) => {
         // using Intl.DateTimeFormat().resolvedOptions().timeZone to get user timezone didn't work with Heroku
         // const userZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-        return moment(date).tz('America/Denver').format('lll');
+        return moment(date).tz('America/Denver').format('lll z');
     },
     truncate: (str, len) => {
         if(str.length > len && str.length > 0) {
