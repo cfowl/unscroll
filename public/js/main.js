3185daf59c65c9eb9de0f58fa7e3e4192574fab6
cfowl <goose_7@msn.com>
2022-12-26 13:54:43 -0700

    testing time zones

--- public/js/main.js
+++ public/js/main.js
@@ -2,12 +2,14 @@ const userZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 let scrollDates = document.querySelectorAll('.scroll-date');
 
 scrollDates.forEach(sd => {
-    let UTCDate = sd.innerHTML;
-    console.log("UTC date: " + UTCDate);
+    // let UTCDate = sd.innerHTML;
+    // console.log("UTC date: " + UTCDate);
 
-    let localDate = new Date(UTCDate).toLocaleString(
-        'en-us',
-        { year:"numeric", month:"short", day:"numeric", hour:"2-digit", minute:"2-digit" }
-    );
-    console.log("Local date: " + localDate);
+    // let localDate = new Date(UTCDate).toLocaleString(
+    //     'en-us',
+    //     { year:"numeric", month:"short", day:"numeric", hour:"2-digit", minute:"2-digit" }
+    // );
+    // console.log("Local date: " + localDate);
+
+    console.log(moment().toString());
 });
