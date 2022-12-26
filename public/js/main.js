a6a1fc87dea9711f7a52d11920618cb6c4ac039d
cfowl <goose_7@msn.com>
2022-12-26 13:28:26 -0700

    testing local time zone from Heroku server

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,11 @@
+const userZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 let scrollDates = document.querySelectorAll('.scroll-date');
 
 scrollDates.forEach(sd => {
-    sd.innerHTML = Intl.DateTimeFormat().resolvedOptions().timeZone;
+    let UTCDate = sd.innerHTML;
+    let localDateTime = new Date(UTCDate).toLocaleString(
+        'en-us',
+        { year:"numeric", month:"short", day:"numeric", hour:"2-digit", minute:"2-digit" }
+    );
+    sd.innerHTML = localDateTime;
 });
