2262c493725db3b039d266cae44346d105437f5a
cfowl <goose_7@msn.com>
2022-12-26 14:15:46 -0700

    fixed import path error

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,4 @@
-import { formatDates } from "../js/modules/dates";
+import { formatDates } from "./modules/dates.js";
 
 // format the dates dynamically outside the Heroku server
 let scrollDates = document.querySelectorAll('.scroll-date');
