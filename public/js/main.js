4eb1a7df836aaba4c1e6851f7fa7b66dbb657984
cfowl <goose_7@msn.com>
2022-12-26 18:11:14 -0700

    hamburger menu toggle

--- public/js/main.js
+++ public/js/main.js
@@ -3,3 +3,12 @@ import { formatDates } from "./modules/dates.js";
 // format the dates dynamically outside the Heroku server
 let scrollDates = document.querySelectorAll('.scroll-date');
 formatDates(scrollDates);
+
+// hamburger nav toggle
+let hambars = document.querySelector('.hambars');
+let navMenu = document.querySelector('.navMenu');
+
+hambars.addEventListener('click', () => {
+    hambars.classList.toggle('open');
+    navMenu.classList.toggle('open');
+});
