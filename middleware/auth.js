d29d4734bc1a3e954a68dc24f8ef4c5e217ea45c
cfowl <goose_7@msn.com>
2023-01-09 23:15:51 -0700

    comments + testing for res.session.requestedURL

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,10 +1,25 @@
 module.exports = {
     ensureAuth: (req, res, next) => {
-        if(req.isAuthenticated()) return next();
-        else res.redirect('/');
+        if(req.isAuthenticated()) {
+            // if user is logged in, continue (to wherever they're trying to go)
+            return next();
+        }
+        else {
+            // otherwise, redirect to login page
+            req.session.requestedURL = req.originalUrl;
+            req.session.save();
+            console.log('------- Requested URL = ' + req.session.requestedURL + ' -------');
+            res.redirect('/');
+        }
     },
     ensureGuest: (req, res, next) => {
-        if(req.isAuthenticated()) res.redirect('/dashboard');
-        else return next();
+        if(req.isAuthenticated()) {
+            // if user is already logged in, redirect to dashboard
+            res.redirect('/dashboard');
+        }
+        else {
+            // otherwise, continue (to login page)
+            return next();
+        }
     }
 };
