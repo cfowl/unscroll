5ca6d170b9877e315c37bd3ee33c0f95906fbc04
cfowl <goose_7@msn.com>
2023-02-04 15:08:33 -0700

    added functionality for unfriending

--- routes/users.js
+++ routes/users.js
@@ -62,9 +62,9 @@ router.put('/:id', ensureAuth, async (req, res) => {
     }
 });
 
-// @desc    Update user's friends
-// @route   PUT /users/:id/friends
-router.put('/:id/friends', ensureAuth, async (req, res) => {
+// @desc    Update user's friends --> pushing a friend
+// @route   PUT /users/:id/friends/push
+router.put('/:id/friends/push', ensureAuth, async (req, res) => {
     try {
 
         let user = await User.findById(req.params.id).lean();
@@ -77,10 +77,33 @@ router.put('/:id/friends', ensureAuth, async (req, res) => {
             // push friend to friends array
             user = await User.findOneAndUpdate(
                 { _id: req.params.id }, 
-                { $push: { friends: req.body.friend } },
+                { $push: { friends: req.body.friendToPush } },
                 {   new: true,
                     runValidators: true
                 }
+            );
+
+            res.redirect(`/users/${user._id}`);
+        }
+    } catch (err) {
+        console.error(err);
+        res.render('error/500');
+    }
+});
+
+// @desc    Update user's friends --> pulling a friend
+// @route   PUT /users/:id/friends/pull/:friendID
+router.put('/:id/friends/pull/:friendID', ensureAuth, async (req, res) => {
+    try {
+        let user = await User.findById(req.params.id).lean();
+        if(!user) return res.render('error/404');
+
+        if(user._id != req.user.id) res.render('error/500');
+        else {
+            // remove friend from friends array
+            user = await User.findOneAndUpdate(
+                { _id: req.params.id }, 
+                { $pull: { friends: req.params.friendID } }
             );
 
             res.redirect(`/users/${user._id}`);
