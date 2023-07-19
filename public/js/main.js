ac5ecff19538cdece6fd442c69ea716da545b651
cfowl <goose_7@msn.com>
2023-07-18 19:28:03 -0600

    added ability to filter favorite scrolls on dash

--- public/js/main.js
+++ public/js/main.js
@@ -70,6 +70,37 @@ enableDashSearch();
 // ------------ //
 enablePublicSearch();
 
+// ++++++++++++ //
+// toggle favorite scrolls
+// ------------ //
+// get favorites button
+let favoritesButton = document.querySelector('#favoritesButton');
+// get the scroll rows from the dashboard table
+let dashScrolls = Array.from(document.getElementsByClassName('dash-scroll'));
+// get array of user favorites
+let favorites = document.querySelector('#user-favorites').innerHTML;
+
+// listen for clicks on the favorites button
+favoritesButton.addEventListener('click', () => {
+    // loop through scrolls
+    dashScrolls.forEach(scroll => {
+        let scrollId = scroll.querySelector('.scroll-id').innerHTML.toString();
+        // if favorites button is active, reset
+        if(favoritesButton.classList.contains('active')) {
+            scroll.classList.remove('favorites-hide');
+        }
+        // else, filter favorites
+        else {
+            if(favorites.includes(scrollId)) {
+                scroll.classList.remove('favorites-hide');
+            } else {
+                scroll.classList.add('favorites-hide');
+            }
+        }
+    });
+    favoritesButton.classList.toggle('active');
+});
+
 
 // ++++++++++++ //
 // toggle search box
